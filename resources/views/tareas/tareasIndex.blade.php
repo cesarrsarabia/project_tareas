1aaf2529477bdf5f6ae14ab38eafd33c02d2934e
Cesar Sarabia <cesar.rsarabia@alumnos.udg.mx>
2020-03-19 16:33:02 -0600

    Uso de layouts
    Tema incluido al proyecto  para separar secciones del html y diseño

--- resources/views/tareas/tareasIndex.blade.php
+++ resources/views/tareas/tareasIndex.blade.php
@@ -1,41 +1,39 @@
-@extends('layouts.app')
+@extends('layouts.tema')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    Listado Tareas
 
-                </div>
-                <div class="card-body">
-                    <a href="{{action('TareaController@create')}}" class="btn btn-success btn-primary">
-                        Nueva Tarea
-                    </a>
-
-                    <hr>
-                    <table class="table">
-                        <thead>
-                            <th>ID</th>
-                            <th>Tarea</th>
-                            <th>Descripcion</th>
-                        </thead>
-                        @foreach ($tareas as $tarea)
-                        <tr>
-                            <td>
-                                <a href="{{route('tarea.show',$tarea->id)}}">
-                                    {{$tarea->id}}
-                                </a>
-                            </td>
-                            <td>{{$tarea->nombre_tarea}}</td>
-                            <td>{{$tarea->descripcion}}</td>
-                        </tr>
-                        @endforeach
-                    </table>
-                </div>
-            </div>
-        </div>
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
     </div>
-</div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <a href="{{action('TareaController@create')}}" class="btn btn-success btn-primary">
+            Nueva Tarea
+        </a>
+
+        <hr>
+        <table class="table">
+            <thead>
+                <th>ID</th>
+                <th>Tarea</th>
+                <th>Descripcion</th>
+            </thead>
+            @foreach ($tareas as $tarea)
+            <tr>
+                <td>
+                    <a href="{{route('tarea.show',$tarea->id)}}">
+                        {{$tarea->id}}
+                    </a>
+                </td>
+                <td>{{$tarea->nombre_tarea}}</td>
+                <td>{{$tarea->descripcion}}</td>
+            </tr>
+            @endforeach
+        </table>
+      </div>
+    </div>
+  </div>
+
+
 @endsection
